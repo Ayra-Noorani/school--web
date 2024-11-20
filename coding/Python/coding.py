ae133301cf8a2475383dc5fcbdab8f60d9dd5c1d
Ayra-Noorani <ayra.n@7hillskampala.com>
2024-11-20 16:03:03 +0300

    Add files via upload

---
+++ coding/Python/coding.py
@@ -0,0 +1,27 @@
+'''
+# A program that adds two numbers
+x = int(input("Enter your first number"))
+y = int(input("Enter your second number"))
+
+pdt = x * y
+print(pdt)
+
+x = ['Daniel','Jeremy','Ayra']
+
+for i in x:
+    print(i)
+
+'''
+#if statements
+
+x=8
+y=9
+
+if x>y:
+    print('This is true')
+else:
+    print('This is false')
+
+
+
+
